5fa9893d46ec1cd9570de1bfebebdcdc976ab86d
Jon Totorica <Jttoto7@gmail.com>
2026-07-08 13:14:31 -0400

    Add intermediate assembler programs to gitignore. Change instruction memeory so loading instructions from hex file happens in the python test file rather than the rtl.

--- rtl/core/instr_mem.sv
+++ rtl/core/instr_mem.sv
@@ -11,9 +11,13 @@ module instr_mem (
     // Initial blocks run exactly once at time zero when sim starts
     // Load mem from a hex file. $readmemh reads line by line and loads each value into mem[0], mem[1]....
     // A hex file is a plain text file where each line is a 32 bit instruction
+
+    /*
+    Instructions are loaded in from python files instead
     initial begin
         $readmemh("program.hex", mem);
     end
+    */
 
     assign instr = mem[addr[9:2]];
 
