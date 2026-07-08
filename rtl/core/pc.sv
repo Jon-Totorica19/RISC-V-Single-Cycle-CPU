c4255c5f43cfa988d5fec278f93650b92e5251a8
Jon Totorica <Jttoto7@gmail.com>
2026-07-08 14:10:19 -0400

    Add reset functionality to PC. Needed to run multiple programs in one simulation time.

--- rtl/core/pc.sv
+++ rtl/core/pc.sv
@@ -1,6 +1,6 @@
 // Program Counter: Holds the current PC value and increments on each clock edge. PC value used to address the instruction memory
 module pc (
-    input logic clk,
+    input logic clk, rst,
     input logic [31:0] next_pc,
     output logic [31:0] pc_addr
 );
@@ -8,6 +8,9 @@ module pc (
     initial pc_addr = 32'd0;
 
     always_ff @(posedge clk) begin
+        if (rst) 
+        pc_addrs <= 0;
+        else
         pc_addr <= next_pc;
     end
 
